1f221f09c99fcfa5cfbdc7b0eb2b1bca46285e8c
Denis Mazurchuk <denis.mazurchuk@gmail.com>
2020-05-23 10:37:34 +0300

    refactored

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,7 +7,39 @@ class Product {
   }
 }
 
-class ShoppingCart {
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId, shouldRender = true) {
+    this.hookId = renderHookId;
+    if (shouldRender) {
+      this.render();
+    }
+  }
+
+  render() {}
+
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
   items = [];
 
   set cartItems(value) {
@@ -25,6 +57,10 @@ class ShoppingCart {
     return sum;
   }
 
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
+
   addProduct(product) {
     const updatedItems = [...this.items];
     updatedItems.push(product);
@@ -32,31 +68,29 @@ class ShoppingCart {
   }
 
   render() {
-    const cartEl = document.createElement("section");
-    cartEl.classList = "cart";
+    const cartEl = this.createRootElement("section", "cart");
     cartEl.innerHTML = `
     <h2>Total: \$${0}</h2>
     <button>Order Now!</button>
     `;
     this.totalOutput = cartEl.querySelector("h2");
-    return cartEl;
   }
 }
 
-class ProductItem {
-  constructor(product) {
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false);
     this.product = product;
+    this.render();
   }
 
   addToCart() {
     console.log(this);
-
     App.addProductToCart(this.product);
   }
 
   render() {
-    const prodEl = document.createElement("li");
-    prodEl.className = "product-item";
+    const prodEl = this.createRootElement("li", "product-item");
     prodEl.innerHTML = `
         <div>
             <img src="${this.product.imageURL}" alt="${this.product.title}">
@@ -70,58 +104,65 @@ class ProductItem {
       `;
     const addCartButton = prodEl.querySelector("button");
     addCartButton.addEventListener("click", this.addToCart.bind(this));
-    return prodEl;
   }
 }
 
-class ProductList {
-  products = [
-    new Product(
-      "A pillow",
-      "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1573576982-amazonbasics-soft-pillow-2-pack-1573576934.jpg",
-      "A soft pillow!",
-      19.99
-    ),
-    new Product(
-      "A carpet",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcShvIv8paIKbRhohHuPR2Dg0wSvXCtsgXRH0uc9Ny9wc0T9Bw0T&usqp=CAU",
-      "A carpet which you might like - or not.",
-      89.99
-    ),
-    new Product(
-      undefined,
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcShvIv8paIKbRhohHuPR2Dg0wSvXCtsgXRH0uc9Ny9wc0T9Bw0T&usqp=CAU",
-      "A carpet which you might like - or not.",
-      89.99
-    ),
-  ];
+class ProductList extends Component {
+  products = [];
 
-  constructor() {}
+  constructor(renderHookId) {
+    super(renderHookId);
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.products = [
+      new Product(
+        "A pillow",
+        "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1573576982-amazonbasics-soft-pillow-2-pack-1573576934.jpg",
+        "A soft pillow!",
+        19.99
+      ),
+      new Product(
+        "A carpet",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcShvIv8paIKbRhohHuPR2Dg0wSvXCtsgXRH0uc9Ny9wc0T9Bw0T&usqp=CAU",
+        "A carpet which you might like - or not.",
+        89.99
+      ),
+      new Product(
+        undefined,
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcShvIv8paIKbRhohHuPR2Dg0wSvXCtsgXRH0uc9Ny9wc0T9Bw0T&usqp=CAU",
+        "A carpet which you might like - or not.",
+        89.99
+      ),
+    ];
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    for (const prod of this.products) {
+      new ProductItem(prod, "prod-list");
+    }
+  }
 
   render() {
-    const prodList = document.createElement("ul");
-    prodList.className = "product-list";
-    // eslint-disable-next-line no-restricted-syntax
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
-      prodList.append(prodEl);
+    this.createRootElement("ul", "product-list", [
+      new ElementAttribute("id", "prod-list"),
+    ]);
+    if (this.products && this.products.length > 0) {
+      this.renderProducts();
     }
-    return prodList;
   }
 }
 
 class Shop {
+  constructor() {
+    this.render();
+  }
+
   render() {
-    const renderHook = document.getElementById("app");
-
-    this.cart = new ShoppingCart();
-    const cartEl = this.cart.render();
-    const productList = new ProductList();
-    const prodListEl = productList.render();
-
-    renderHook.append(cartEl);
-    renderHook.append(prodListEl);
+    this.cart = new ShoppingCart("app");
+    new ProductList("app");
   }
 }
 
@@ -130,7 +171,6 @@ class App {
 
   static init() {
     const shop = new Shop();
-    shop.render();
     this.cart = shop.cart;
   }
 
